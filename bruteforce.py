edace4c3575948d993d940d97556b8c7518f2d33
AmirSasson <amirsas@gmail.com>
2022-09-29 10:18:34 +0300

    fix comment

--- bruteforce.py
+++ bruteforce.py
@@ -15,7 +15,7 @@ def TryPassword(text)->bool:
 
 for מספר in range(100000):
     stringNumber = str(מספר) # להפוך מספר לטקסט
-    passwordCandidate = stringNumber.zfill(5) # לרפד באפסים ,לדוגמא המספר 1 יהיה "000001" כמספר הספרות של הססמא
+    passwordCandidate = stringNumber.zfill(5) # לרפד באפסים ,לדוגמא המספר 1 יהיה "00001" כמספר הספרות של הססמא
     print(passwordCandidate) # נדפיס למסך את הקוד שאנחנו רוצים לנסות
     if TryPassword(passwordCandidate): # ננסה את הססמא
         print(f'your code is : {passwordCandidate}') # הצלחנו!!
